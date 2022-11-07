9d6f234ac888fea450caec82bbd8d2ec5100101d
GyeongEun Kim <kekim20@naver.com>
2022-11-08 00:28:28 +0900

    initial project setting

---
+++ src/main/java/sillenceSoft/schedulleCall/SchedulleCallApplication.java
@@ -0,0 +1,13 @@
+package sillenceSoft.schedulleCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulleCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulleCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sillenceSoft/schedulleCall/SchedulleCallApplicationTests.java
@@ -0,0 +1,13 @@
+package sillenceSoft.schedulleCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulleCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
